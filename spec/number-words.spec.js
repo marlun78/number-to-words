9c5e12fb4776412bf1f45f9e66a3c7f0132b1565
Martin Eneqvist <marlun78@hotmail.com>
2015-02-08 16:38:03 +0100

    Adding tests

--- spec/number-words.spec.js
+++ spec/number-words.spec.js
@@ -1,3 +1,69 @@
 'use strict';
 
 var numberWords = require('../src/number-words');
+
+describe('numberWords', function () {
+    var tests = [
+        { input: 0, expect: 'zero' },
+        { input: 1, expect: 'one' },
+        { input: 2, expect: 'two' },
+        { input: 3, expect: 'three' },
+        { input: 4, expect: 'four' },
+        { input: 5, expect: 'five' },
+        { input: 6, expect: 'six' },
+        { input: 7, expect: 'seven' },
+        { input: 8, expect: 'eight' },
+        { input: 9, expect: 'nine' },
+        { input: 10, expect: 'ten' },
+        { input: 11, expect: 'eleven' },
+        { input: 12, expect: 'twelve' },
+        { input: 13, expect: 'thirteen' },
+        { input: 14, expect: 'fourteen' },
+        { input: 15, expect: 'fifteen' },
+        { input: 16, expect: 'sixteen' },
+        { input: 17, expect: 'seventeen' },
+        { input: 18, expect: 'eighteen' },
+        { input: 19, expect: 'nineteen' },
+        { input: 20, expect: 'twenty' },
+        { input: 22, expect: 'twenty two' },
+        { input: 33, expect: 'thirty three' },
+        { input: 44, expect: 'forty four' },
+        { input: 55, expect: 'fifty five' },
+        { input: 66, expect: 'sixty six' },
+        { input: 77, expect: 'seventy seven' },
+        { input: 88, expect: 'eighty eight' },
+        { input: 99, expect: 'ninety nine' },
+        { input: 111, expect: 'one hundred eleven' },
+        { input: 2222, expect: 'two thousand two hundred twenty two' },
+        { input: 33333, expect: 'thirty three thousand three hundred thirty three' },
+        { input: 444444, expect: 'four hundred forty four thousand four hundred forty four' },
+        { input: 5555555, expect: 'five million five hundred fifty five thousand five hundred fifty five' },
+        { input: 66666666, expect: 'sixty six million six hundred sixty six thousand six hundred sixty six' },
+        { input: 777777777, expect: 'seven hundred seventy seven million seven hundred seventy seven thousand seven hundred seventy seven' },
+        { input: 8888888888, expect: 'eight billion eight hundred eighty eight million eight hundred eighty eight thousand eight hundred eighty eight' },
+        { input: 99999999999, expect: 'ninety nine billion nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine' },
+        { input: 111111111111, expect: 'one hundred eleven billion one hundred eleven million one hundred eleven thousand one hundred eleven' },
+        { input: 2222222222222, expect: 'two trillion two hundred twenty two billion two hundred twenty two million two hundred twenty two thousand two hundred twenty two' },
+        { input: 33333333333333, expect: 'thirty three trillion three hundred thirty three billion three hundred thirty three million three hundred thirty three thousand three hundred thirty three' },
+        { input: 444444444444444, expect: 'four hundred forty four trillion four hundred forty four billion four hundred forty four million four hundred forty four thousand four hundred forty four' },
+        { input: 5555555555555555, expect: 'five quadrillion five hundred fifty five trillion five hundred fifty five billion five hundred fifty five million five hundred fifty five thousand five hundred fifty five' },
+        { input: 9007199254740992, expect: 'nine quadrillion seven trillion one hundred ninety nine billion two hundred fifty four million seven hundred forty thousand nine hundred ninety two' }
+    ];
+    
+    function addTest(test) {
+        it('should, if passed ' + formatNumber(test.input) + ', return ' + test.expect, function () {
+            expect(numberWords(test.input)).toEqual(test.expect);
+        });
+    }
+
+    tests.forEach(addTest);
+});
+
+function formatNumber(number) {
+    var result = String(number).split('').reverse().map(function (num, index) {
+        if (index % 3 === 2) return '.' + num;
+        return num;
+    }).reverse();
+    var length = result.length;
+    return result.join('') + ' (' + length + ')';
+}
--- src/number-words.js
+++ src/number-words.js
@@ -1,4 +1,4 @@
-'use strict'; 
+'use strict';
 
 module.exports = numberWords;
 
@@ -15,36 +15,43 @@ var ONE_TRILLION    = 1000000000000;    // 1.000.000.000.000 (12)
 var ONE_QUADRILLION = 1000000000000000; // 1.000.000.000.000.000 (15)
 var MAX             = 9007199254740992; // 9.007.199.254.740.992
 
-var lessThanTwenty = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 
-    'ten', 'eleven', 'twelv', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-var lessThanOneHundred = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninty'];
-var isFinite_ = Number.isFinite || function (value) {
-    return typeof value === "number" && isFinite(value);
-};
+var lessThanTwenty = [
+    'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+];
+var lessThanOneHundred = [
+    null, null, 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+];
+
+if (typeof Number.isFinite !== 'function') {
+    Number.isFinite = function isFinite(value) {
+        return typeof value === 'number' && isFinite(value);
+    };
+}
 
 function numberWords(number) {
     return toWords(number);
 }
 
-var iterations = 0
+//var iterations = 0;
 function toWords(number, words) {
-    if (!isFinite_(number)) throw TypeError;
-    if (!words) words = [];
-    iterations++;
-    console.log(iterations, number, words);
-
     var remainder, currentWord;
-    if (number === 0) {
-        return words.join(' ');
-        
-    } else if (number < 20) {
+
+    //iterations++;
+    //console.log(iterations, number, words);
+
+    if (!Number.isFinite(number)) throw TypeError;
+    if (number === 0) return words ? words.join(' ') : lessThanTwenty[0];
+    if (!words) words = [];
+
+    if (number < 20) {
         remainder = 0;
         currentWord = lessThanTwenty[number];
-		
+
     } else if (number < ONE_HUNDRED) {
         remainder = number % TEN;
         currentWord = lessThanOneHundred[Math.floor(number / TEN)];
-		
+
     } else if (number < ONE_THOUSAND) {
         remainder = number % ONE_HUNDRED;
         currentWord = toWords(Math.floor(number / ONE_HUNDRED)) + ' hundred';
@@ -52,7 +59,7 @@ function toWords(number, words) {
     } else if (number < ONE_MILLION) {
         remainder = number % ONE_THOUSAND;
         currentWord = toWords(Math.floor(number / ONE_THOUSAND)) + ' thousand';
-		
+
     } else if (number < ONE_BILLION) {
         remainder = number % ONE_MILLION;
         currentWord = toWords(Math.floor(number / ONE_MILLION)) + ' million';
@@ -60,17 +67,17 @@ function toWords(number, words) {
     } else if (number < ONE_TRILLION) {
         remainder = number % ONE_BILLION;
         currentWord = toWords(Math.floor(number / ONE_BILLION)) + ' billion';
-		
+
     } else if (number < ONE_QUADRILLION) {
         remainder = number % ONE_TRILLION;
         currentWord = toWords(Math.floor(number / ONE_TRILLION)) + ' trillion';
-		
+
     } else if (number <= MAX) {
         remainder = number % ONE_QUADRILLION;
         currentWord = toWords(Math.floor(number / ONE_QUADRILLION)) + ' quadrillion';
-		
+
     }
-    
+
     words.push(currentWord);
     return toWords(remainder, words);
 }
