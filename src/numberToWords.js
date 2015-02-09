150856cd8c7730e8b74332fc7bdac1768b8d061b
Martin Eneqvist <marlun78@users.noreply.github.com>
2015-02-09 09:27:38 +0100

    Update numberToWords.js

--- src/numberToWords.js
+++ src/numberToWords.js
@@ -6,21 +6,20 @@ module.exports = numberToWords;
 
 var numberWordsToOrdinal = require('./numberWordsToOrdinal');
 
-var TEN = 10;
-var ONE_HUNDRED = 100;
-var ONE_THOUSAND = 1000;             // 1.000 (3)
-var ONE_MILLION = 1000000;          // 1.000.000 (6)
-var ONE_BILLION = 1000000000;       // 1.000.000.000 (9)
-var ONE_TRILLION = 1000000000000;    // 1.000.000.000.000 (12)
+var TEN             = 10;
+var ONE_HUNDRED     = 100;
+var ONE_THOUSAND    = 1000;             // 1.000 (3)
+var ONE_MILLION     = 1000000;          // 1.000.000 (6)
+var ONE_BILLION     = 1000000000;       // 1.000.000.000 (9)
+var ONE_TRILLION    = 1000000000000;    // 1.000.000.000.000 (12)
 var ONE_QUADRILLION = 1000000000000000; // 1.000.000.000.000.000 (15)
-var MAX = 9007199254740992; // 9.007.199.254.740.992
-
-var lessThanTwenty = [
+var MAX             = 9007199254740992; // 9.007.199.254.740.992
+var LESS_THAN_20 = [
     'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
     'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
 ];
-var lessThanOneHundred = [
-    null, null, 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+var THENTHS_LESS_THAN_100 = [
+    'zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
 ];
 
 if (typeof Number.isFinite !== 'function') {
@@ -39,16 +38,16 @@ function toWords(number, words) {
 
     if (!Number.isFinite(number)) throw TypeError;
     // If number is 0 and words is undefined, return 'zero' else return all the words
-    if (number === 0) return !words ? lessThanTwenty[0] : words.join(' ');
+    if (number === 0) return !words ? LESS_THAN_20[0] : words.join(' ');
     if (!words) words = [];
 
     if (number < 20) {
         remainder = 0;
-        currentWord = lessThanTwenty[number];
+        currentWord = LESS_THAN_20[number];
 
     } else if (number < ONE_HUNDRED) {
         remainder = number % TEN;
-        currentWord = lessThanOneHundred[Math.floor(number / TEN)];
+        currentWord = THENTHS_LESS_THAN_100[Math.floor(number / TEN)];
 
     } else if (number < ONE_THOUSAND) {
         remainder = number % ONE_HUNDRED;
@@ -73,7 +72,6 @@ function toWords(number, words) {
     } else if (number <= MAX) {
         remainder = number % ONE_QUADRILLION;
         currentWord = toWords(Math.floor(number / ONE_QUADRILLION)) + ' quadrillion';
-
     }
 
     words.push(currentWord);
