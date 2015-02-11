ebc5d60bc22ce20e9f1e57ed4ce73a9de6aff3dd
Martin Eneqvist <marlun78@hotmail.com>
2015-02-11 22:51:49 +0100

    Fixing format with dashes and commas

--- src/numberToWords.js
+++ src/numberToWords.js
@@ -3,23 +3,24 @@
 module.exports = numberToWords;
 
 // TODO: Performance optimizations (MAX takes 32 calls)
-// TODO: Add commas and dashes (like; nine quadrillion, seven trillion, one hundred ninety-nine billion, two hundred fifty-four million, seven hundred forty thousand, nine hundred ninety-two)
 
 var numberWordsToOrdinal = require('./numberWordsToOrdinal');
 
-var TEN             = 10;
-var ONE_HUNDRED     = 100;
-var ONE_THOUSAND    = 1000;             // 1.000 (3)
-var ONE_MILLION     = 1000000;          // 1.000.000 (6)
-var ONE_BILLION     = 1000000000;       // 1.000.000.000 (9)
-var ONE_TRILLION    = 1000000000000;    // 1.000.000.000.000 (12)
+var TEN = 10;
+var ONE_HUNDRED = 100;
+var ONE_THOUSAND = 1000;
+var ONE_MILLION = 1000000;
+var ONE_BILLION = 1000000000;           //         1.000.000.000 (9)
+var ONE_TRILLION = 1000000000000;       //     1.000.000.000.000 (12)
 var ONE_QUADRILLION = 1000000000000000; // 1.000.000.000.000.000 (15)
-var MAX             = 9007199254740992; // 9.007.199.254.740.992
-var LESS_THAN_20 = [
+var MAX = 9007199254740992;             // 9.007.199.254.740.992 (15)
+
+var LESS_THAN_TWENTY = [
     'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
     'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
 ];
-var THENTHS_LESS_THAN_100 = [
+
+var TENTHS_LESS_THAN_HUNDRED = [
     'zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
 ];
 
@@ -30,51 +31,68 @@ if (typeof Number.isFinite !== 'function') {
 }
 
 function numberToWords(number, asOrdinal) {
+    if (!Number.isFinite(number)) {
+        throw TypeError;
+    }
     var words = toWords(number);
     return asOrdinal ? numberWordsToOrdinal(words) : words;
 }
 
 function toWords(number, words) {
-    var remainder, currentWord;
+    var remainder, word;
 
-    if (!Number.isFinite(number)) throw TypeError;
-    // If number is 0 and words is undefined, return 'zero' else return all the words
-    if (number === 0) return !words ? LESS_THAN_20[0] : words.join(' ');
-    if (!words) words = [];
+    // We’re done
+    if (number === 0) {
+        return !words ? 'zero' : words.join(' ').replace(/,$/, '');
+    }
+    // First run
+    if (!words) {
+        words = [];
+    }
+    // If negative, prepend “minus”
+    if (number < 0) {
+        words.push('minus');
+        number = Math.abs(number);
+    }
 
     if (number < 20) {
         remainder = 0;
-        currentWord = LESS_THAN_20[number];
+        word = LESS_THAN_TWENTY[number];
 
     } else if (number < ONE_HUNDRED) {
         remainder = number % TEN;
-        currentWord = THENTHS_LESS_THAN_100[Math.floor(number / TEN)];
+        word = TENTHS_LESS_THAN_HUNDRED[Math.floor(number / TEN)];
+        // In case of remainder, we need to handle it here to be able to add the “-”
+        if (remainder) {
+            word += '-' + LESS_THAN_TWENTY[remainder];
+            remainder = 0;
+        }
 
     } else if (number < ONE_THOUSAND) {
         remainder = number % ONE_HUNDRED;
-        currentWord = toWords(Math.floor(number / ONE_HUNDRED)) + ' hundred';
+        word = toWords(Math.floor(number / ONE_HUNDRED)) + ' hundred';
 
     } else if (number < ONE_MILLION) {
         remainder = number % ONE_THOUSAND;
-        currentWord = toWords(Math.floor(number / ONE_THOUSAND)) + ' thousand';
+        word = toWords(Math.floor(number / ONE_THOUSAND)) + ' thousand,';
 
     } else if (number < ONE_BILLION) {
         remainder = number % ONE_MILLION;
-        currentWord = toWords(Math.floor(number / ONE_MILLION)) + ' million';
+        word = toWords(Math.floor(number / ONE_MILLION)) + ' million,';
 
     } else if (number < ONE_TRILLION) {
         remainder = number % ONE_BILLION;
-        currentWord = toWords(Math.floor(number / ONE_BILLION)) + ' billion';
+        word = toWords(Math.floor(number / ONE_BILLION)) + ' billion,';
 
     } else if (number < ONE_QUADRILLION) {
         remainder = number % ONE_TRILLION;
-        currentWord = toWords(Math.floor(number / ONE_TRILLION)) + ' trillion';
+        word = toWords(Math.floor(number / ONE_TRILLION)) + ' trillion,';
 
     } else if (number <= MAX) {
         remainder = number % ONE_QUADRILLION;
-        currentWord = toWords(Math.floor(number / ONE_QUADRILLION)) + ' quadrillion';
+        word = toWords(Math.floor(number / ONE_QUADRILLION)) + ' quadrillion,';
     }
 
-    words.push(currentWord);
+    words.push(word);
     return toWords(remainder, words);
 }
--- src/numberWordsToOrdinal.js
+++ src/numberWordsToOrdinal.js
@@ -2,11 +2,11 @@
 
 module.exports = numberWordsToOrdinal;
 
-var endsWith00 = /(hundred|thousand|(m|b|tr|quadr)illion)$/;
+var endsWithDoubleZero = /(hundred|thousand|(m|b|tr|quadr)illion)$/;
 var endsWithTeen = /teen$/;
 var endsWithY = /y$/;
-var endsWith1Through12 = /( ?)(one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve)$/;
-var ordinalLessThan13 = {
+var endsWithOneThroughTwelve = /(one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve)$/;
+var ordinalLessThanThirteen = {
     one: 'first',
     two: 'second',
     three: 'third',
@@ -23,7 +23,7 @@ var ordinalLessThan13 = {
 
 function numberWordsToOrdinal(words) {
     // Ends with *00 (100, 1000, etc.) or *teen (13, 14, 15, 16, 17, 18, 19)
-    if (endsWith00.test(words) || endsWithTeen.test(words)) {
+    if (endsWithDoubleZero.test(words) || endsWithTeen.test(words)) {
         return words + 'th';
     }
     // Ends with *y (20, 30, 40, 50, 60, 70, 80, 90)
@@ -31,9 +31,9 @@ function numberWordsToOrdinal(words) {
         return words.replace(endsWithY, 'ieth');
     }
     // Ends with one through twelve
-    else if (endsWith1Through12.test(words)) {
-        return words.replace(endsWith1Through12, function (match, space, numberWord) {
-            return space ? '-' + ordinalLessThan13[numberWord] : ordinalLessThan13[numberWord];
+    else if (endsWithOneThroughTwelve.test(words)) {
+        return words.replace(endsWithOneThroughTwelve, function (match, numberWord) {
+            return ordinalLessThanThirteen[numberWord];
         });
     }
     return words;
