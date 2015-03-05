fc0e947f169dc5a52955969035edd469974731e2
Martin Eneqvist <marlun78@hotmail.com>
2015-03-05 22:31:40 +0100

    Added spec for external API

---
+++ spec/indexSpec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var index = require('../src');
+
+describe('index', function () {
+    it('should expose a toOrdinal method', function () {
+        expect(index.toOrdinal).toEqual(jasmine.any(Function));
+    });
+    it('should expose a toWords method', function () {
+        expect(index.toWords).toEqual(jasmine.any(Function));
+    });
+    it('should expose a toWordsOrdinal method', function () {
+        expect(index.toWordsOrdinal).toEqual(jasmine.any(Function));
+    });
+});
